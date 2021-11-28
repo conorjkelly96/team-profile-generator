833974644fee1153440045f0d6b6d47eaf6f3330
Conor Kelly <conorjkelly96@gmail.com>
2021-11-28 16:41:26 +0000

    comitting changes to html template

--- src/util.js
+++ src/util.js
@@ -1,109 +1,62 @@
 // generate manager card
-const generateManagerCard = () => {
-  return `<div
-    class="
-    uk-card
-    uk-card-default
-    uk-width-1-2@m
-    uk-card
-    uk-card-secondary
-    uk-card-hover
-    uk-card-body
-    uk-light
-    uk-margin-medium
-    "
->
-    <div class="uk-card-header">
-    <div class="uk-grid-small uk-flex-middle" uk-grid>
-        <div class="uk-width-auto">
-        <i class="fa-solid fa-graduation-cap"></i>
+const generateManagerCard = (manager) => {
+  return `<div class="col">
+    <div class="card" style="width: 18rem">
+        <div class="card-body">
+        <h5 class="card-title">${manager.name}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Manager 👨‍🏫</h6>
+        <h6 class="card-subtitle mb-2 text-muted">${manager.id}</h6>
+        <p class="card-text">
+            If you'd like to get in touch or have issues regarding
+            escalations, please reach out to me below.
+        </p>
+        <div class="d-flex flex-row flex-wrap justify-content-center">
+            <a href="${manager.email}" class="card-link text-decoration-none">${manager.email}</a>
+            <a href="#" class="card-link text-decoration-none">${manager.officeNumber}</a>
         </div>
-        <div class="uk-width-expand">
-        <h3 class="uk-card-title uk-margin-remove-bottom">
-            Michael Jordan
-        </h3>
-        <p class="uk-text-meta uk-margin-remove-top">Intern</p>
         </div>
-    </div>
-    </div>
-    <div class="uk-card-footer">
-    <a href="#" class="uk-button uk-button-text">St. Benedict's</a>
-    <a href="#" class="uk-button uk-button-text">EID: 3286793</a>
-    <a href="#" class="uk-button uk-button-text">mj@soton.ac.uk</a>
     </div>
     </div>`;
 };
 
 // generate intern card
-const generateInternCard = () => {
-  return `<div
-        class="
-        uk-card
-        uk-card-default
-        uk-width-1-2@m
-        uk-card
-        uk-card-secondary
-        uk-card-hover
-        uk-card-body
-        uk-light
-        uk-margin-medium
-        "
-    >
-        <div class="uk-card-header">
-        <div class="uk-grid-small uk-flex-middle" uk-grid>
-            <div class="uk-width-auto">
-            <i class="fa-solid fa-graduation-cap"></i>
-            </div>
-            <div class="uk-width-expand">
-            <h3 class="uk-card-title uk-margin-remove-bottom">
-                Michael Jordan
-            </h3>
-            <p class="uk-text-meta uk-margin-remove-top">Intern</p>
-            </div>
+const generateInternCard = (intern) => {
+  return `<div class="col">
+    <div class="card" style="width: 18rem">
+        <div class="card-body">
+        <h5 class="card-title">${intern.name} </h5>
+        <h6 class="card-subtitle mb-2 text-muted">Intern 🧑🏽‍🎓</h6>
+        <h6 class="card-subtitle mb-2 text-muted">${intern.id}</h6>
+        <p class="card-text">
+            Hi there! If you'd like to get in touch, please reach out to
+            me below.
+        </p>
+        <div class="d-flex flex-row flex-wrap justify-content-center">
+        <a href="${intern.email}" class="card-link text-decoration-none">${intern.email}</a>
+        <a href="#" class="card-link text-decoration-none">College: ${intern.school}</a>
         </div>
         </div>
-        <div class="uk-card-footer">
-        <a href="#" class="uk-button uk-button-text">St. Benedict's</a>
-        <a href="#" class="uk-button uk-button-text">EID: 3286793</a>
-        <a href="#" class="uk-button uk-button-text">mj@soton.ac.uk</a>
-        </div>
+    </div>
     </div>`;
 };
 
 // generate engineer card
-const generateEngineerCard = () => {
-  return `<div
-    class="
-        uk-card
-        uk-card-default
-        uk-width-1-2@m
-        uk-card
-        uk-card-secondary
-        uk-card-hover
-        uk-card-body
-        uk-light
-        uk-margin-medium
-    "
-    >
-    <div class="uk-card-header">
-        <div class="uk-grid-small uk-flex-middle" uk-grid>
-        <div class="uk-width-auto">
-            <i class="fa-solid fa-screwdriver-wrench"></i>
-        </div>
-        <div class="uk-width-expand">
-            <h3 class="uk-card-title uk-margin-remove-bottom">
-            Conor Kelly
-            </h3>
-            <p class="uk-text-meta uk-margin-remove-top">Engineer</p>
+const generateEngineerCard = (engineer) => {
+  return `<div class="col">
+    <div class="card" style="width: 18rem">
+        <div class="card-body">
+        <h5 class="card-title">${engineer.name}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Engineer 🧑‍🔬</h6>
+        <h6 class="card-subtitle mb-2 text-muted">${engineer.id}</h6>
+        <p class="card-text">
+            Want to check out my work and collaborate on a new project?
+            Click the links below!
+        </p>
+        <div class="d-flex flex-row flex-wrap justify-content-center">
+        <a href="${engineer.email}" class="card-link text-decoration-none">${engineer.email}</a>
+        <a href="#" class="card-link text-decoration-none">GitHub: ${engineer.github}</a>
         </div>
         </div>
-    </div>
-    <div class="uk-card-footer">
-        <a href="#" class="uk-button uk-button-text">GitHub</a>
-        <a href="#" class="uk-button uk-button-text">EID: 3286793</a>
-        <a href="#" class="uk-button uk-button-text"
-        >conorjkelly96@gmail.com</a
-        >
     </div>
     </div>`;
 };
